e7f72366d974948eb361518ca2b069a7b2bafd4e
ludel <ludovic.delsol@ynov.com>
2020-11-30 00:27:37 +0100

    Command-line for recommendations systems

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ La valeur de ce score est comprise entre la note minimale et la note maximale de
 Pour calculer le score des produits fournis dans le dataset, exécutez la commande suivante :  
 `python3 compute_product_review_score.py --data=/path/to/dataset/directory`  
 Ce script créra dans le dossier du dataset un fichier csv apellé `olist_product_scores.csv` qui contiendra les scores associés aux produits.
+
+## Recommandations par preduit et client
+Ce script permet de générer des recommandations pour une produit ou un client en particulier. Les deux systèmes utilisent un algorithme qui regroupe les clients similaires afin de leurs proposés des produits en fonction leurs centres d'intérêt.
+
+#### Recommandation client
+`python3 compute_recommendations.py --data=/path/to/dataset/directory --client-id=971bf8f42a9f8cb3ead257854905b454`
+
+
+#### Recommandation produit
+`python3 compute_recommendations.py --data=/path/to/dataset/directory --client-id=971bf8f42a9f8cb3ead257854905b454 --product-id=595fac2a385ac33a80bd5114aec74eb8`
+
+#### Arguments optionnels
+`--import-clustered-data` permet d'utiliser les clusters générés précedament.
+
+`--nb-cluster` permet de définir le nombre de cluster (60 par défaut).
+
+`--limit` permet de définir le nombre maximum de recommandations (20 par défaut).
--- src/run/compute_recommendations.py
+++ src/run/compute_recommendations.py
@@ -1,35 +1,84 @@
+import argparse
+from collections import Counter
+
 import pandas as pd
 from sklearn.cluster import KMeans
 
 
 class ComputeRecommendation:
-    def __init__(self, nb_cluster=60, data_path='../../data'):
+    def __init__(self, nb_cluster: int, data_path: str, limit: int):
         self.df_clustered = None
         self.nb_cluster = nb_cluster
         self.data_path = data_path
+        self.limit = limit
 
-    def get_full_merged_df(self):
         df_orders = pd.read_csv(f'{self.data_path}/olist_orders_dataset.csv')
         df_orders_item = pd.read_csv(f'{self.data_path}/olist_order_items_dataset.csv')
         df_product = pd.read_csv(f'{self.data_path}/olist_products_dataset.csv')
-        return pd.merge(df_orders, df_orders_item, on='order_id').merge(df_product, on='product_id')
+        self.df = pd.merge(df_orders, df_orders_item, on='order_id').merge(df_product, on='product_id')
 
     def create_cluster(self):
-        df = self.get_full_merged_df()
-        df_cat = df.groupby('customer_id')['product_category_name'].value_counts().unstack(fill_value=0)
+        df_cat = self.df.groupby('customer_id')['product_category_name'].value_counts().unstack(fill_value=0)
 
         y_pred = KMeans(random_state=5, n_clusters=self.nb_cluster).fit_predict(df_cat)
         cluster_df = pd.DataFrame(data={'customer_id': df_cat.index, 'cluster': y_pred})
 
-        self.df_clustered = pd.merge(df, cluster_df, on='customer_id')
+        self.df_clustered = pd.merge(self.df, cluster_df, on='customer_id')
+        self.export_df()
+
+    def get_cluster_group(self, current_user: str) -> pd.DataFrame:
+        assert self.df_clustered is not None, 'Clustered dataframe is not generated.'
+
+        customer = self.df_clustered.loc[self.df_clustered['customer_id'] == current_user]
+        return self.df_clustered.loc[self.df_clustered['cluster'] == customer['cluster'].iloc[0]]
+
+    def predict_for_product(self, current_user: str, current_product: str) -> list:
+        cluster_group = self.get_cluster_group(current_user)
+
+        orders_with_same_product = cluster_group.loc[cluster_group['product_id'] == current_product, 'order_id']
+        recommended_products = []
+        for order_id in orders_with_same_product:
+            order = self.df_clustered.loc[self.df_clustered['order_id'] == order_id]
+            for product in order.loc[order['product_id'] != current_product, 'product_id']:
+                recommended_products.append(product)
+
+        return sorted(Counter(recommended_products))[:self.limit]
+
+    def predict_for_client(self, current_user: str) -> list:
+        cluster_group = self.get_cluster_group(current_user)
+        return sorted(Counter(cluster_group['product_id']))[:self.limit]
 
     def import_df(self):
-        self.df_clustered = pd.read_csv('../../data/full_clustered.csv')
+        self.df_clustered = pd.read_csv(f'{self.data_path}/full_clustered.csv')
 
     def export_df(self):
-        self.df_clustered.to_csv('../../data/full_clustered.csv')
+        print('Export clustered data ...')
+        self.df_clustered.to_csv(f'{self.data_path}/full_clustered.csv')
 
 
 if __name__ == '__main__':
-    compute = ComputeRecommendation()
-    compute.create_cluster()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', help='Path to data directory', required=True)
+    parser.add_argument('--nb-cluster', help='Number of cluster (default:60)', default=60)
+    parser.add_argument('--limit', help='Limit of product (default:20)', default=20)
+    parser.add_argument('--import-clustered-data', help='Use clustered dataframe previously generated',
+                        action="store_true")
+    parser.add_argument('--client-id', required=True)
+    parser.add_argument('--product-id', required=False)
+    args = parser.parse_args()
+
+    compute = ComputeRecommendation(args.nb_cluster, args.data, args.limit)
+    print('Loading data ...')
+    if args.import_clustered_data:
+        compute.import_df()
+    else:
+        compute.create_cluster()
+
+    if args.product_id:
+        print('Recommendation for a specific product')
+        recommendation = compute.predict_for_product(args.client_id, args.product_id)
+    else:
+        print('Recommendation for a client')
+        recommendation = compute.predict_for_client(args.client_id)
+
+    print('Products recommendation :', ', '.join(recommendation))
